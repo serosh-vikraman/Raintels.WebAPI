e2b701f4170dce7eb04cf2bac2e42c1898ba6095
serosh k vikraman <serosh.vikraman@outlook.com>
2021-06-07 10:03:38 +0530

    Serilog Implementation

--- Raintels.Web/Controllers/StudentController.cs
+++ Raintels.Web/Controllers/StudentController.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Raintels.Entity.ViewModel;
 using Raintels.Service.ServiceInterface;
+using Serilog;
 
 namespace Raintels.Web.API.Controllers
 {
@@ -82,7 +85,9 @@ namespace Raintels.Web.API.Controllers
         [HttpPost("savestudent")]
         public void SaveStudent(StudentViewModel student)
         {
+            Log.Information("SaveStudent");
             studentService.CreateStudent(student);
+            Log.Information("EndSaveStudent");
         }
 
         [HttpPost("updatestudent")]
@@ -95,6 +100,36 @@ namespace Raintels.Web.API.Controllers
         public void DeleteStudent(int studentId)
         {
             studentService.DeleteStudent(studentId);
+        }
+
+        public async Task<IActionResult> Upload()
+        {
+            try
+            {
+                var formCollection = await Request.ReadFormAsync();
+                var file = formCollection.Files.First();
+                var folderName = Path.Combine("Resources", "Images");
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    var fullPath = Path.Combine(pathToSave, fileName);
+                    var dbPath = Path.Combine(folderName, fileName);
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                    return Ok(new { dbPath });
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
         }
     }
 }
--- Raintels.Web/Program.cs
+++ Raintels.Web/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
 
 namespace Raintels.Web.API
 {
@@ -7,11 +9,30 @@ namespace Raintels.Web.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+               .Enrich.FromLogContext()
+               .WriteTo.Console()
+               .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Minute)
+               .CreateLogger();
+            try
+            {
+                Log.Information("Starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+             .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
